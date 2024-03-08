1529826b2900593b140f3621eb42529983565ed5
LightningRhino <oc+github@mailbox.org>
2024-03-08 15:03:22 +0100

    change

--- zhist.sh
+++ zhist.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-zpool history > /home/olleg/.scripts/zpool-history.txt && sed -i '/trim tank/d;/scrub/d;/zfs-auto-snap/d;/autosnap/d;/unallow/d;/allow/d;/import/d;/shutdown/d' /home/olleg/.scripts/zpool-history.txt
+zpool history > /home/olleg/.scripts/zpool-history.txt && sed -i '/trim tank/d;/scrub/d;/zfs-auto-snap/d;/autosnap/d;/unallow/d;/allow/d;/import/d;/export/d;/shutdown/d' /home/olleg/.scripts/zpool-history.txt
