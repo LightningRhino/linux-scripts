ffc969dbcde23d6af35c5daee53efd64a962447a
LightningRhino <oc+github@mailbox.org>
2024-08-29 15:31:19 +0200

    Updating scripts

--- borg2nas.sh
+++ borg2nas.sh
@@ -5,5 +5,5 @@ set -x
 # oder
 # 192.168.178.201:/volume1/backup/borg/end /mnt/borg2nas nfs defaults,noauto 0 0
 mount /mnt/borg2nas
-/usr/bin/borg create -s --progress --list --filter AME -C zstd --exclude-from bexclude.txt /mnt/borg2nas::$(hostname -s)-$(date +%Y-%m-%d,%H:%M) /home/olleg/essentials /home/olleg/.scripts /home/olleg/.bashrc /home/olleg/docker /home/olleg/.config /root /etc /var/lib/libvirt /home/olleg/git /home/olleg/.local /tank/vagrant/machines
+/usr/bin/borg create -s --progress --list --filter AME -C zstd --exclude-from bexclude.txt /mnt/borg2nas::$(hostname -s)-$(date +%Y-%m-%d,%H:%M) /home/olleg/essentials /home/olleg/.scripts /home/olleg/.bashrc /home/olleg/docker /home/olleg/.config /root /etc /var/lib/libvirt /home/olleg/git /home/olleg/.local
 umount /mnt/borg2nas
