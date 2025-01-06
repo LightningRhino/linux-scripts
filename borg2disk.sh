14a4f35b303ff7a582a8260e4c50d0fc19530c01
LightningRhino <oc+github@mailbox.org>
2025-01-06 16:06:33 +0100

    Add /usr/share/edk2 to borg backup

--- borg2disk.sh
+++ borg2disk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-/usr/bin/borg create -s --progress --list --filter AME -C zstd --exclude-from bexclude.txt /mnt/usbdisk/borg2disk::$(hostname -s)-$(date +%Y-%m-%d,%H:%M) /home/olleg/essentials /home/olleg/.scripts /home/olleg/.bashrc /home/olleg/docker /home/olleg/.config /root /etc /var/lib/libvirt /home/olleg/git /home/olleg/.local
+/usr/bin/borg create -s --progress --list --filter AME -C zstd --exclude-from bexclude.txt /mnt/usbdisk/borg2disk::$(hostname -s)-$(date +%Y-%m-%d,%H:%M) /home/olleg/essentials /home/olleg/.scripts /home/olleg/.bashrc /home/olleg/docker /home/olleg/.config /root /etc /usr/share/edk2/x64 /var/lib/libvirt /home/olleg/git /home/olleg/.local
